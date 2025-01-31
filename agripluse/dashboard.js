70eeb2d6a42f0589dccaab70969a197f4179c1a8
Maniprasad1503 <maniprasad1503@gmail.com>
2025-01-31 17:23:35 +0530

    Add files via upload

---
+++ agripluse/dashboard.js
@@ -0,0 +1,5 @@
+// Add click events to grid items
+document.querySelectorAll('.grid-item').forEach(item => {
+    item.addEventListener('click', () => {
+    });
+  });
---
+++ agripluse/forgot.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ForgotPassword from './ForgotPassword';
+
+function App() {
+  return (
+    <div>
+      <ForgotPassword />
+    </div>
+  );
+}
