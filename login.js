94daee80445035ab4a139a9d37867cda18409103
Sanika-Jentli <sanikaj14.jgs@gmail.com>
2025-01-31 17:44:38 +0530

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-
-function handleLogin() {
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  console.log('Email:', email, 'Password:', password);
-  // Add login logic (e.g., API calls) here
-}
