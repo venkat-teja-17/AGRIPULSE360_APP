658b48aeddb27adb6735473810d3fc060bfcb34b
VyshnnaviRaiDesam <vyshnnavirai.desam@gmail.com>
2025-02-04 17:30:45 +0530

    Delete animation.js

--- animation.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  // The intro animation runs when the page loads.
-  setTimeout(() => {
-    // After the animation completes (2 seconds), redirect to the main app page (or next screen).
-    window.location.href = 'login.html'; // Replace with your app's main page URL.
-  }, 7000); // Wait 3 seconds (adjust time if needed)
-};
